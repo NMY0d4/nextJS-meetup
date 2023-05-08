f7af18dc581778746bb2c0d16f9f62586a88c2fd
Grégory Marini <greg.marini@hotmail.fr>
2023-05-08 15:25:07 +0200

    test api route, it's ok

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,19 +6,17 @@ import { MongoClient } from 'mongodb';
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-
     const client = await MongoClient.connect(process.env.MONGO_URI);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
 
     const result = await meetupsCollection.insertOne(data);
-
     console.log(result);
 
     client.close();
 
-    res.status(200).json(result);
+    res.status(200).json(result.insertedId);
   }
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch(`/api/new-meetup`, {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    await response.json();
+
+    router.push('/');
   }
+
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
